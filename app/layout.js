3e8182a9e7aa0664530d4686650ce26638327521
@vanshkansal25 <kansalv578@gmail.com>
2025-07-03 22:22:10 +0530

    Add header component and authentication middleware; update package dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({subsets : ["latin"]})
 
@@ -10,12 +12,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.className}`}
-      >
+        >
+        {/* header */}
+        <Header/>
+        <main className="min-h-screen">
         {children}
+        </main>
+        {/* footer */}
+        <footer className=" py-12 border-neutral-400 border-t-2">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p className="font-bold">Made with 💗 by Vansh Kansal</p>
+            </div>
+          </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
