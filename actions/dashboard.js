6e1e8856d553f103374990f4e7fb9e00cd2b1f8d
@vanshkansal25 <kansalv578@gmail.com>
2025-07-12 11:22:57 +0530

    Implement user authentication in createAccount function; add error handling for unauthorized access and user not found scenarios

---
+++ actions/dashboard.js
@@ -0,0 +1,63 @@
+"use server";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+// this function because next.js cannot handle decimals and this is to convert it back to number so serializing it 
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
+  return serialized;
+};
+
+export async function createAccount(data) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    //convert balance to float before saving
+    const balanceFloat = parseFloat(data.balance)
+    if(isNaN(balanceFloat)) throw new Error("Invalid Balance Amount");
+    
+    const existingAccounts = await db.account.findMany({
+      where: { userId: user.id },
+    });
+    // because user can have many accounts we are checking to select the default account
+    const shouldBeDefault = existingAccounts.length===0?true:data.isDefault;
+    // If this account should be default, unset other default accounts
+    if(shouldBeDefault){
+       await db.account.updateMany({
+        where: { userId: user.id, isDefault: true },
+        data: { isDefault: false },
+      });
+    }
+     // Create new account
+    const account = await db.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldBeDefault, // Override the isDefault based on our logic
+      },
+    });
+     // Serialize the account before returning
+    const serializedAccount = serializeTransaction(account);
+
+    revalidatePath("/dashboard");//helps us to refectch the value of the page all api call of this page will be made again
+    return { success: true, data: serializedAccount };
+
+
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -11,7 +11,7 @@ export default function NotFound() {
         moved.
       </p>
       <Link href="/">
-        <Button>Return Home</Button>
+        <Button className={`cursor-pointer`}>Return Home</Button>
       </Link>
     </div>
   );
