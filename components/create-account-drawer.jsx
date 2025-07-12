32b89d80cb6d2f8939441382b53b275bc0e330eb
@vanshkansal25 <kansalv578@gmail.com>
2025-07-12 12:29:25 +0530

    Add CreateAccountDrawer component and integrate it into the DashboardPage; update package dependencies for form handling and validation

---
+++ components/create-account-drawer.jsx
@@ -0,0 +1,93 @@
+"use client";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { accountSchema } from "@/app/lib/schema";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const CreateAccountDrawer = ({ children }) => {
+  const [open, setOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+    reset,
+  } = useForm({
+    resolver: zodResolver(accountSchema),
+    defaultValues: {
+      name: "",
+      type: "CURRENT",
+      balance: "",
+      isDefault: false,
+    },
+  });
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Create New Account</DrawerTitle>
+        </DrawerHeader>
+        <div className="px-4 pb-4">
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <label
+                htmlFor="name"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Account Name
+              </label>
+              <Input
+                id="name"
+                placeholder="e.g., Main Checking"
+                {...register("name")}
+              />
+              {errors.name && (
+                <p className="text-sm text-red-500">{errors.name.message}</p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="type"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Account Type
+              </label>
+              <Select onValueChange={(value)=>setValue("type",value)}
+                defaultValue={watch("type")}>
+                <SelectTrigger id="type">
+                  <SelectValue placeholder="Select Type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="CURRENT">Current</SelectItem>
+                  <SelectItem value="SAVINGS">Savings</SelectItem>
+                </SelectContent>
+              </Select>
+              {errors.type && (
+                <p className="text-sm text-red-500">{errors.type.message}</p>
+              )}
+            </div>
+          </form>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default CreateAccountDrawer;
