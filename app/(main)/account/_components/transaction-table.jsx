4882b1efa9e55767052652839c9f61017726b1e1
@vanshkansal25 <kansalv578@gmail.com>
2025-07-13 12:57:53 +0530

    Enhance TransactionTable component with sorting and checkbox functionality; add defaultCategories data for transaction categorization

--- app/(main)/account/_components/transaction-table.jsx
+++ app/(main)/account/_components/transaction-table.jsx
@@ -1,37 +1,83 @@
+"use client"
 import React from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-const TransactionTable = ({ transaction }) => {
+import { Checkbox } from "@/components/ui/checkbox";
+import { format } from "date-fns";
+import { categoryColors } from "@/data/category";
+const TransactionTable = ({ transactions }) => {
+    const filteredAndSortedTransactions = transactions
+    const handleSort = ()=>{
+
+    }
   return (
-    <div>
+    <div className="space-y-4">
       {/* Filters */}
       {/* Transactions */}
+      <div>
+
       <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="w-[50px]">
+                <Checkbox/>
+            </TableHead>
+            <TableHead className="cursor-pointer" 
+            onClick={()=>{handleSort("date")}}>
+                <div className="flex items-center">Date</div>
+                </TableHead>
+            <TableHead className="text-right">Description</TableHead>
+            <TableHead className="cursor-pointer" 
+            onClick={()=>{handleSort("category")}}><div className="flex items-center">Category</div></TableHead>
+            <TableHead className="cursor-pointer" 
+            onClick={()=>{handleSort("amount")}}><div className="flex items-center justify-end">Amount</div></TableHead>
+            <TableHead>Recurring</TableHead>
+            <TableHead className="w-[50px]"/>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
+            {filteredAndSortedTransactions.length === 0?(
+                <TableRow>
+                    <TableCell colSpan={7} className="text-center text-muted-foreground">
+                        No Transactions Found
+                    </TableCell>
+                </TableRow>
+            ):(
+                filteredAndSortedTransactions.map((transaction)=>(
+
+                    <TableRow key={transaction.id}>
+            <TableCell>
+                <Checkbox/>
+            </TableCell>
+            <TableCell>{format(new Date(transaction.date),"PP")}</TableCell>
+            <TableCell>{transaction.description}</TableCell>
+            <TableCell className="capitalize">
+                <span style={{
+                    background:categoryColors[transaction.category]
+                }}
+                className="px-2 py-1 rounded text-white text-sm">
+                {transaction.category}
+                </span>
+                </TableCell>
+            <TableCell className="text-right font-medium" style={{
+                color: transaction.type=="EXPENSE"?"red":"green"
+            }}>
+                {transaction.type == "EXPENSE" ? "-":"+"}
+                ${transaction.amount.toFixed(2)}
+                </TableCell>
+            
           </TableRow>
+        ))
+        )}
         </TableBody>
       </Table>
+      </div>
     </div>
   );
 };
