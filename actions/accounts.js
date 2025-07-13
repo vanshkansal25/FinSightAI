1f3ed3ea9503e61507e4719823cdc89438d1a85b
@vanshkansal25 <kansalv578@gmail.com>
2025-07-13 12:13:28 +0530

    Implement getAccountWithTransactions function and create TransactionTable component for account details display

--- actions/accounts.js
+++ actions/accounts.js
@@ -50,3 +50,35 @@ export async function updateDefaultAccount(accountId) {
     return { success: false, error: error.message };
   }
 }
+export async function getAccountWithTransactions(accountId){
+  const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+  const account = await db.account.findUnique({
+    where:{
+      id:accountId,
+      userId:user.id
+    },
+    include: {
+      transactions: {
+        orderBy: { date: "desc" },
+      },
+      _count: {
+        select: { transactions: true },
+      },
+    }, 
+  })
+  if (!account) return null;
+
+  return {
+    ...serializeDecimal(account),
+    transactions: account.transactions.map(serializeDecimal),
+  };
+}
---
+++ app/(main)/account/[id]/page.jsx
@@ -0,0 +1,54 @@
+import { getAccountWithTransactions } from '@/actions/accounts'
+import { notFound } from 'next/navigation';
+import React, { Suspense } from 'react'
+import { BarLoader } from 'react-spinners';
+import TransactionTable from '../_components/transaction-table';
+
+const AccountsPage = async({params}) => {
+  const accountData = await getAccountWithTransactions(params.id);
+  if(!accountData){
+    notFound();
+  }
+
+  const {transactions ,...account} = accountData;
+  return (
+    <div className="space-y-8 px-5">
+      <div className="flex gap-4 items-end justify-between">
+        <div>
+          <h1 className="text-5xl sm:text-6xl font-bold pb-6 bg-gradient-to-br from-blue-600 to-purple-600 tracking-tighter pr-2 text-transparent bg-clip-text capitalize">
+            {account.name}
+          </h1>
+          <p className="text-muted-foreground">
+            {account.type.charAt(0) + account.type.slice(1).toLowerCase()}{" "}
+            Account
+          </p>
+        </div>
+
+        <div className="text-right pb-2">
+          <div className="text-xl sm:text-2xl font-bold">
+            ${parseFloat(account.balance).toFixed(2)}
+          </div>
+          <p className="text-sm text-muted-foreground">
+            {account._count.transactions} Transactions
+          </p>
+        </div>
+      </div>
+
+      {/* Chart Section */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions} />
+      </Suspense>
+
+      {/* Transactions Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <TransactionTable transactions={transactions} />
+      </Suspense>
+    </div>
+  );
+}
+
+export default AccountsPage
