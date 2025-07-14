bbf5f01ac66aa524b3957f4ff125f541e2fde90f
@vanshkansal25 <kansalv578@gmail.com>
2025-07-14 21:32:25 +0530

    Add BudgetProgress component to display budget information on the dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,22 @@ import { Card, CardContent } from '@/components/ui/card'
 import { Plus } from 'lucide-react'
 import React from 'react'
 import AccountCard from './_components/account-card'
+import BudgetProgress from './_components/budget-progress'
 
 const DashboardPage = async() => {
   const accounts = await getUserAccounts();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
   return (
     <div className='px-5'>
         {/* Budget Progess */}
+        {defaultAccount && <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}/> }
         {/* Overview */}
         {/* AccountGrid */}
         <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
