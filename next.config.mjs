28d1dd8c8cb3441e1f5b414f7de2e81e9aba75b2
@vanshkansal25 <kansalv578@gmail.com>
2025-07-27 11:01:14 +0530

    feat: implement email sending functionality and transaction management features And Gemini Api for reciept scanning

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,12 @@ const nextConfig = {
         hostname: "randomuser.me",
       },
     ],
-}
+},
+experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
